49093671243ab3d9041e579600e3ff368b88a6db
notvoltyyy <mmsenkiv021@gmail.com>
2026-04-05 13:14:23 +0300

    login fix v1

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,26 +1,27 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { loginDto } from './Dto/loginDto';
 import { refreshTokenDto } from './Dto/RefreshTokenDto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('login')
-    login(@Body() dto:loginDto){
-        return this.authService.login(dto)
-    }
+  login(@Body() dto: loginDto) {
+    return this.authService.login(dto)
+  }
 
   @Post('refresh')
-  refresh(@Body() dto:refreshTokenDto){
+  refresh(@Body() dto: refreshTokenDto) {
     return this.authService.refresh(dto)
   }
 
-//   @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'))
   @Post('logout')
-  logout(@Request() req){
-    const userId=req.user.userId
+  logout(@Request() req) {
+    const userId = req.user.userId
     return this.authService.logout(userId)
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.enableCors({
-    origin:"*",
-    methods:"GET,HEAD,PUT,PATCH,DELETE,POST",
-    credentials:true
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,DELETE,POST',
+    credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
-  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
