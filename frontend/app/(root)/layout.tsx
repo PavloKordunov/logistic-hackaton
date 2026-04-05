8e5dab9eb3e4d5283ad2e61755dfa8d2ef343793
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2026-04-05 23:08:31 +0300

    final final final v2

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -12,20 +12,16 @@ export default function MainLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    // Змінено min-h-screen на h-screen
     <div className="flex h-screen w-full overflow-hidden bg-military-black text-slate-200 font-sans">
       <SideBar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
-      {/* Обгортка для правого блоку */}
       <div className="flex-1 flex min-w-0 flex-col relative h-full">
         <div className="absolute inset-0 tactical-grid opacity-50 pointer-events-none" />
 
-        {/* Хедер завжди зверху */}
-        <div className="relative z-10">
+        <div className="">
           <Header onToggleSidebar={() => setSidebarOpen((prev) => !prev)} />
         </div>
 
-        {/* Скролиться ТІЛЬКИ main контент, а не вся сторінка */}
         <main className="flex-1 overflow-y-auto relative z-10 custom-scrollbar">
           {children}
         </main>
